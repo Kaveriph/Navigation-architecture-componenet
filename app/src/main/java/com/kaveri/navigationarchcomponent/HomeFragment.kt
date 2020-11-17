b77454e7ed388f4b8238d007079629129550c02f
Kaveriph <kaveri.hindi@gmail.com>
2020-11-17 20:38:12 +0530

    Included the databinding andded navigation access to navigate from one fragment to another

---
+++ app/src/main/java/com/kaveri/navigationarchcomponent/HomeFragment.kt
@@ -0,0 +1,37 @@
+package com.kaveri.navigationarchcomponent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.kaveri.navigationarchcomponent.databinding.FragmentHomeBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.button.setOnClickListener {
+            val args = Bundle()
+            args.putString("TEXT", binding.editTextTextPersonName2.text.toString())
+            it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment, args)
+        }
+    }
+}
---
+++ app/src/main/java/com/kaveri/navigationarchcomponent/SecondFragment.kt
@@ -0,0 +1,38 @@
+package com.kaveri.navigationarchcomponent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.kaveri.navigationarchcomponent.databinding.FragmentSecondBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SecondFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SecondFragment : Fragment() {
+    lateinit var binding:FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        showBundleData(savedInstanceState)
+    }
+
+    private fun showBundleData(savedInstanceState: Bundle?) {
+        val args = arguments?.getString("TEXT") ?: "No text"
+        binding.textView2.text = args
+    }
+
+}
